a280b768ace81cca2f96455500bee632a703c651
0xRudolf <0xrudolf@protonmail.com>
2021-11-25 12:57:54 +0100

    :dizzy: add tokenomics animated chart

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,6 +164,152 @@
       nextBig.addClass("fa-chevron-right");
     }, 400);
   });
+
+  //Tokenomics pie chart
+  const tokenomics = document.getElementById("tokenomics-pie");
+  const dataset = JSON.parse(tokenomics.dataset.shares);
+  const labels = JSON.parse(tokenomics.dataset.labels);
+  const colors = JSON.parse(tokenomics.dataset.colors);
+
+  const width = tokenomics.offsetWidth;
+  const height = tokenomics.offsetHeight;
+  const minOfWH = Math.min(width, height) / 2;
+  const initialAnimDelay = 300;
+  const arcAnimDelay = 150;
+  const arcAnimDur = 2000;
+  const secDur = 1000;
+  const secIndividualdelay = 150;
+
+  let radius;
+
+  // calculate minimum of width and height to set chart radius
+  if (minOfWH > 200) {
+    radius = 200;
+  } else {
+    radius = minOfWH;
+  }
+
+  // append svg
+  let svg = d3
+    .select("#tokenomics-pie")
+    .append("svg")
+    .attr({
+      width: width,
+      height: height,
+      class: "pieChart",
+    })
+    .append("g");
+
+  svg.attr({
+    transform: `translate(${width / 2}, ${height / 2})`,
+  });
+
+  // for drawing slices
+  let arc = d3.svg
+    .arc()
+    .outerRadius(radius * 0.65)
+    .innerRadius(radius * 0.25);
+
+  // for labels and polylines
+  let outerArc = d3.svg
+    .arc()
+    .innerRadius(radius * 0.75)
+    .outerRadius(radius * 0.75);
+
+  let pie = d3.layout.pie().value((d) => d);
+
+  let draw = function () {
+    svg.append("g").attr("class", "lines");
+    svg.append("g").attr("class", "slices");
+    svg.append("g").attr("class", "labels");
+
+    // define slice
+    let slice = svg
+      .select(".slices")
+      .datum(dataset)
+      .selectAll("path")
+      .data(pie);
+    slice
+      .enter()
+      .append("path")
+      .attr({
+        fill: (d, i) => colors[i],
+        d: arc,
+        "stroke-width": "25px",
+        transform: (d, i) => "rotate(-180, 0, 0)",
+      })
+      .style("opacity", 0)
+      .transition()
+      .delay((d, i) => i * arcAnimDelay + initialAnimDelay)
+      .duration(arcAnimDur)
+      .ease("elastic")
+      .style("opacity", 1)
+      .attr("transform", "rotate(0,0,0)");
+
+    slice
+      .transition()
+      .delay((d, i) => arcAnimDur + i * secIndividualdelay)
+      .duration(secDur)
+      .attr("stroke-width", "5px");
+
+    let midAngle = (d) => d.startAngle + (d.endAngle - d.startAngle) / 2;
+
+    let text = svg.select(".labels").selectAll("text").data(pie(dataset));
+
+    text
+      .enter()
+      .append("text")
+      .attr("dy", "0.35em")
+      .style("opacity", 0)
+      .style("fill", (d, i) => colors[i])
+      .text((d, i) => labels[i])
+      .attr("transform", (d) => {
+        // calculate outerArc centroid for 'this' slice
+        let pos = outerArc.centroid(d);
+        // define left and right alignment of text labels
+        pos[0] = radius * 0.75 * (midAngle(d) < Math.PI ? 1 : -1);
+        return `translate(${pos})`;
+      })
+      .style("text-anchor", (d) => (midAngle(d) < Math.PI ? "start" : "end"))
+      .transition()
+      .delay((d, i) => arcAnimDur + i * secIndividualdelay)
+      .duration(secDur)
+      .style("opacity", 1);
+
+    let polyline = svg
+      .select(".lines")
+      .selectAll("polyline")
+      .data(pie(dataset));
+
+    polyline
+      .enter()
+      .append("polyline")
+      .style("opacity", 0.5)
+      .attr("points", (d) => {
+        let pos = outerArc.centroid(d);
+        pos[0] = radius * 0.7 * (midAngle(d) < Math.PI ? 1 : -1);
+        return [arc.centroid(d), arc.centroid(d), arc.centroid(d)];
+      })
+      .transition()
+      .duration(secDur)
+      .delay((d, i) => arcAnimDur + i * secIndividualdelay)
+      .attr("points", (d) => {
+        let pos = outerArc.centroid(d);
+        pos[0] = radius * 0.7 * (midAngle(d) < Math.PI ? 1 : -1);
+        return [arc.centroid(d), outerArc.centroid(d), pos];
+      });
+  };
+
+  const tokenomicsObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.intersectionRatio > 0) {
+        draw();
+        tokenomicsObserver.unobserve(entry.target);
+      } else {
+      }
+    });
+  });
+  tokenomicsObserver.observe(tokenomics);
 })(jQuery);
 
 //Add snow effect

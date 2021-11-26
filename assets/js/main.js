2466bb99786086d6d1e4b1e64d6c04d7175bcab1
0xRudolf <0xrudolf@protonmail.com>
2021-11-27 00:51:34 +0100

    :sparkles: update presale with live countdown

--- assets/js/main.js
+++ assets/js/main.js
@@ -337,6 +337,48 @@
     });
   });
   tokenomicsObserver.observe(tokenomics);
+
+  //Countdown
+  const presaleCountdown = document.getElementById("presale-countdown");
+  const startTime = presaleCountdown.dataset.timestamp;
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+    dd = String(today.getDate()).padStart(2, "0"),
+    mm = String(today.getMonth() + 1).padStart(2, "0");
+
+  //end
+
+  const countDown = parseInt(startTime),
+    x = setInterval(function () {
+      const now = new Date().getTime(),
+        distance = countDown - now;
+
+      (document.getElementById("presale-days").innerText = Math.floor(
+        distance / day
+      )),
+        (document.getElementById("presale-hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("presale-minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("presale-seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+
+      //do something later when date is reached
+      if (distance < 0) {
+        presaleCountdown.innerHTML = "";
+        clearInterval(x);
+      }
+      //seconds
+    }, 1000);
 })(jQuery);
 
 //Add snow effect
